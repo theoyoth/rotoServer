98d166fe8af2f75bf63215eb0ef09d49424f1cda
theoyoth <theobantang1@gmail.com>
2022-03-15 15:49:46 +0700

    menghapus yang di comment

--- roto-server/server/index.js
+++ roto-server/server/index.js
@@ -23,15 +23,7 @@ app.use(
 )
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-//   )
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, authorization')
-//   next()
-// })
+
 app.use(rute)
 
 module.exports = {
